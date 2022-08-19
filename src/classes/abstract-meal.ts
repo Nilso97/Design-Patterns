f62129c1a8fe28d28a219c08527d110404c5cae9
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-08-19 06:41:27 -0300

    Delete src/classes directory

--- src/classes/abstract-meal.ts
+++
@@ -1,9 +0,0 @@
-import { IMealCompositeProtocol } from "../interfaces/meal-composite-protocol";
-
-export abstract class AbstractMeal implements IMealCompositeProtocol{
-    constructor(private name: string, private price: number) {}
-    
-    getPrice(): number {
-        return this.price;
-    }
-}
